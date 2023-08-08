48898142b966a04fd0ff2c3259f2046fe33573aa
Saqlain451 <jobs.sakil.mustak@gmail.com>
2023-08-08 11:05:42 +0530

    add task loading fixed

--- client/src/Components/AddTask.jsx
+++ client/src/Components/AddTask.jsx
@@ -3,12 +3,12 @@ import { useState } from "react";
 import Cookies from "js-cookie";
 import axios from "axios";
 import { toast } from "react-toastify";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar.jsx";
 
 const AddTask = () => {
   const { api } = useGlobalHook();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [addData, setAddData] = useState({
     taskTitle: "",
     taskDes: "",
@@ -49,43 +49,50 @@ const AddTask = () => {
 
   return (
     <>
-      <Navbar/>
-      <div className="d-flex center" style={{width:"100%", height:"100vh"}}>
-      <div className="modal-box">
-        <label htmlFor="taskTitle">Title</label>
-        <input
-          type="text"
-          name="taskTitle"
-          className={"form-control mt-1 pl-1 pr-1"}
-          style={{ border: "2px solid #EDEFF2" }}
-          value={addData.taskTitle}
-          onChange={changeChandle}
-        />
-        <label htmlFor="taskDes">Description</label>
-        <textarea
-          name="taskDes"
-          className={" form-control textarea mt-1"}
-          style={{ border: "2px solid #EDEFF2" }}
-          value={addData.taskDes}
-          onChange={changeChandle}
-        ></textarea>
-        <label htmlFor="Starttime">Start time of Task</label>
-        <input
-          type="datetime-local"
-          name={"startTime"}
-          className={"form-control mt-1 mb-2 pl-1 pr-1"}
-          style={{ border: "2px solid #EDEFF2" }}
-          value={addData.startTime}
-          onChange={changeChandle}
-        />
+      <Navbar />
+      <div className="d-flex center" style={{ width: "100%", height: "100vh" }}>
+        <div className="modal-box">
+          <label htmlFor="taskTitle">Title</label>
+          <input
+            type="text"
+            name="taskTitle"
+            className={"form-control mt-1 pl-1 pr-1"}
+            style={{ border: "2px solid #EDEFF2" }}
+            value={addData.taskTitle}
+            onChange={changeChandle}
+          />
+          <label htmlFor="taskDes">Description</label>
+          <textarea
+            name="taskDes"
+            className={" form-control textarea mt-1"}
+            style={{ border: "2px solid #EDEFF2" }}
+            value={addData.taskDes}
+            onChange={changeChandle}
+          ></textarea>
+          <label htmlFor="Starttime">Start time of Task</label>
+          <input
+            type="datetime-local"
+            name={"startTime"}
+            className={"form-control mt-1 mb-2 pl-1 pr-1"}
+            style={{ border: "2px solid #EDEFF2" }}
+            value={addData.startTime}
+            onChange={changeChandle}
+          />
 
-        <button
-          className={"btn-add pointer mx-auto fs-1"}
-          onClick={submitHandler}
-        >
-          {isLoading ? <span className={"btn-loader"}></span> : "Add Task"}
-        </button>
-      </div>
+          <button
+            className={"btn-add pointer mx-auto fs-1 d-flex center"}
+            onClick={submitHandler}
+          >
+            {isLoading ? (
+              <span
+                className={"btn-loader"}
+                style={{ width: "2rem", height: "2rem" }}
+              ></span>
+            ) : (
+              "Add Task"
+            )}
+          </button>
+        </div>
       </div>
     </>
   );
